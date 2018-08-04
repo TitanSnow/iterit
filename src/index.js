361dfc5dd606fda135311f13893405722dd1443e
TitanSnow <sweeto@live.cn>
2018-08-04 20:43:53 +0800

    add iter

--- src/index.js
+++ src/index.js
@@ -28,6 +28,64 @@ function decorateFunction(origin, decorator, length = 0) {
     return decorated
 }
 
-export function curry(f) {
-    return decorateFunction(f, f => (...args) => arg1 => f(arg1, ...args), '-1')
+export function curry() {
+    return f => decorateFunction(f, f => (...args) => arg1 => f(arg1, ...args), '-1')
 }
+
+export const index = function index(obj, key) {
+    return obj[key]
+} |> curry()
+
+export const bind = function bind(func, obj) {
+    return func.bind(obj)
+} |> curry()
+
+export const getBound = function getBound(obj, key) {
+    return obj
+        |> index(key)
+        |> bind(obj)
+} |> curry()
+
+const getIteratorFactory = function getIteratorFactory(obj) {
+    return obj
+        |> getBound(Symbol.iterator)
+} |> curry()
+
+export const typeOf = function typeOf(obj) {
+    return typeof obj
+} |> curry()
+
+export const is = function is(a, b) {
+    return Object.is(a, b)
+} |> curry()
+
+export const isIterable = function isIterable(obj) {
+    return obj
+        |> getIteratorFactory()
+        |> typeOf()
+        |> is('function')
+} |> curry()
+
+export const call = function call(func, ...args) {
+    return func(...args)
+} |> curry()
+
+export const iter = function iter(obj) {
+    if (!(obj |> isIterable())) {
+        throw new TypeError('object not iterable')
+    } else {
+        return obj
+            |> getIteratorFactory()
+            |> call()
+    }
+} |> curry()
+
+export const isIterator = function isIterator(obj) {
+    if (!(obj |> isIterable())) {
+        return false
+    } else {
+        return obj
+            |> iter()
+            |> is(obj)
+    }
+} |> curry()
