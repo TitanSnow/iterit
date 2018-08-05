c2af5abf44331a973a4be4581102c3f17d9a78bc
TitanSnow <sweeto@live.cn>
2018-08-05 11:11:17 +0800

    add next, range, times, drop, reduce

--- src/index.js
+++ src/index.js
@@ -129,6 +129,10 @@ export const entries = function entries(obj) {
     }
 } |> curry()
 
+export const isNullish = function isNullish(obj) {
+    return obj === null || obj === void 0
+} |> curry()
+
 export const not = function not(obj) {
     return !obj
 } |> curry()
@@ -160,4 +164,60 @@ export const concat = function* concat(...its) {
             yield item
         }
     }
+} |> curry()
+
+export const next = function next(it) {
+    return it.next()
+} |> curry()
+
+export const range = function* range(...args) {
+    let start, stop, step
+    switch (args.length) {
+        case 1:
+            [stop] = args
+            start = 0
+            step = 1
+            break
+        case 2:
+            [start, stop] = args
+            step = 1
+            break
+        case 3:
+            [start, stop, step] = args
+            break
+        default:
+            throw new TypeError('invalid arguments')
+    }
+    for (let i = start; step > 0 ? i < stop : i > stop ; i += step) {
+        yield i
+    }
+}
+
+export const times = function times(func, times) {
+    for (const i of range(times)) {
+        func()
+    }
+} |> curry()
+
+export const drop = function drop(it, n = 1) {
+    it = it |> iter()
+    it |> getBound('next') |> times(n)
+    return it
+} |> curry()
+
+export const reduce = function reduce(it, func, initialValue) {
+    it = it |> iter()
+    let accumulator
+    let idx
+    if (initialValue |> isNullish()) {
+        accumulator = it |> next() |> index('value')
+        idx = 1
+    } else {
+        accumulator = initialValue
+        idx = 0
+    }
+    for (const item of it) {
+        accumulator = func(accumulator, item, idx++, it)
+    }
+    return accumulator
 } |> curry()
